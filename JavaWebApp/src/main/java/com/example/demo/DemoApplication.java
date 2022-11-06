bddd95c8d6826f90720011e6748238c2cf150717
kimsy400 <kimsy400@yahoo.com>
2022-11-05 21:31:36 -0600

    fix

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -1,18 +1,16 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class DemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-
-		String name = "kemisola";
-		System.output.println(name);
-
-		
-	}
-
+	
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        String name = "vamsi";
+        System.out.println(name);
+    }
 }
