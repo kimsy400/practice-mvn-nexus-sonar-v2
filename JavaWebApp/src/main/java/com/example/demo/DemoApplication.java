7ab5cc681b268f2cc48d3079d808bba5daed63e5
kimsy400 <99833523+kimsy400@users.noreply.github.com>
2022-11-05 21:46:27 -0600

    Update DemoApplication.java

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,6 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         String name = "kemisola";
-        System.out.println(name):
+        System.output.println(name);
     }
 }
